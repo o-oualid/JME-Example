583b2ab3ccdc6a29f0790aba0a12ecb31eae6f15
ouazrou  oualid <qwertyoualid@gmail.com>
2018-01-13 15:57:47 +0000

    main release

---
+++ android/src/main/java/com/jme/game/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jme.game;
+
+import android.os.Bundle;
+
+import com.jme3.app.AndroidHarness;
+
+public class MainActivity extends AndroidHarness {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        app=new Main();
+        app.start();
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ core/src/com/jme/game/Main.java
@@ -0,0 +1,34 @@
+package com.jme.game;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
+public class Main extends SimpleApplication {
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+
+        Material mat = new Material(assetManager,
+                "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+        rootNode.attachChild(geom);
+    }
+
+    @Override
+
+    public void simpleUpdate(float tpf) {
+
+    }
+
+    @Override
+
+    public void simpleRender(RenderManager rm) {
+
+    }
+}
