e9f9f409cc9cbff0c5353a133256094031088f8c
ouazrou  oualid <qwertyoualid@gmail.com>
2018-01-16 19:46:39 +0000

    fix asset not found for Desktop Ios and Vr.

--- core/src/main/java/com/jme/game/Main.java
+++ core/src/main/java/com/jme/game/Main.java
@@ -7,6 +7,7 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 
 // your game need to be on this package
@@ -14,16 +15,14 @@ import com.jme3.scene.shape.Box;
 public class Main extends SimpleApplication {
     @Override
     public void simpleInitApp() {
-       Box b = new Box(1, 1, 1);                     // create box mesh
-        Geometry geom = new Geometry("Box", b);       // create geometry from mesh
-
-        Material mat = new Material(assetManager,
-                "Common/MatDefs/Misc/Unshaded.j3md"); // create a simple material
-        mat.setColor("Color", ColorRGBA.Blue);        // color the material blue
-        geom.setMaterial(mat);                        // assign the material to geometry
-        rootNode.attachChild(geom);
-        //Spatial model = assetManager.loadModel("model.obj");
-       // rootNode.attachChild(model);
+      /* Box b = new Box(1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+        Material mat = new Material(assetManager,"Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+        rootNode.attachChild(geom);*/
+        Spatial model = assetManager.loadModel("model.obj");
+        rootNode.attachChild(model);
         DirectionalLight sun = new DirectionalLight();
         sun.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)));
         sun.setColor(ColorRGBA.White);
